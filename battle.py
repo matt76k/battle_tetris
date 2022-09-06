afa4f0deee72d0cbfe56cf0f10b0bcd30b5762b4
matt <matt76k@gmail.com>
2022-09-07 08:23:21 +0900

    Add Battle Mode

---
+++ battle.py
@@ -0,0 +1,95 @@
+import curses
+from board import ACTIONS, Board
+from renderer import RenderBoard, init_colors
+import asyncio
+import websockets
+import json
+import time
+
+players = {}
+num_players = 0
+start = asyncio.Condition()
+lock = asyncio.Lock()
+
+async def join(ws, name):
+    global num_players, lock, start
+    async with lock:
+        num_players += 1
+    board = Board()
+    viewer = RenderBoard(board, player2=True if num_players == 2 else False)
+    players[ws] = {'name': name, 'board': board, 'viewer': viewer, 'score': 0}
+    async with start:
+        if num_players == 2:
+            start.notify_all()
+        else:
+            await start.wait()
+
+async def make_info(ws):
+    global players
+
+    board1 = None
+    board2 = None
+
+    for k, v in players.items():
+        if k == ws:
+            board1 = v['board'].__dict__
+        else:
+            board2 = v['board'].__dict__
+
+    return [board1, board2]
+    
+
+async def handler(ws):
+    global players
+
+    async for message in ws:
+        message = json.loads(message)
+        e = message['event']
+        player = players.get(ws, None)
+
+        if e == 'join':
+            await join(ws, message['name'])
+            player = players[ws]
+            info = json.dumps(await make_info(ws))
+            await ws.send(json.dumps({'event': 'act', 'info': info}))
+
+        elif e == 'act':
+            player['board'].act(ACTIONS.index(message['act']))
+            info = json.dumps(await make_info(ws))
+
+            if player['board'].is_game_over():
+                player['score'] = player['board'].score
+                player['viewer'].draw_game_window()
+                await ws.send(json.dumps({'event': 'gameover', 'info': json.dumps(await make_info(ws))}))
+                return 
+
+            player['viewer'].draw_game_window()
+
+            if player['score'] != player['board'].score:
+                player['viewer'].draw_status_window()
+                player['score'] = player['board'].score
+
+            await ws.send(json.dumps({'event': 'act', 'info': info}))
+
+def init_render():
+    curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    curses.start_color()
+    curses.curs_set(0)
+
+    init_colors()
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    import warnings
+    warnings.filterwarnings('ignore')
+    init_render()
+
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        curses.endwin()
---
+++ human.py
@@ -0,0 +1,55 @@
+import curses
+from board import ACTIONS, Board, Block
+from renderer import RenderBoard, init_colors
+import asyncio
+import websockets
+import json
+import sys
+
+scr = curses.initscr()
+curses.noecho()
+curses.cbreak()
+curses.start_color()
+curses.curs_set(0)
+init_colors()
+
+game_window = curses.newwin(20, 20, 0, 0)
+game_window.nodelay(True)
+game_window.keypad(1)
+
+async def main():
+
+
+    async with websockets.connect("ws://localhost:8765") as ws:
+        await ws.send(json.dumps({'event': 'join', 'name': 'human'}))
+        async for message in ws:
+            message = json.loads(message)
+
+            e = message['event']
+            if e == 'act':
+                act = None
+                while act is None:
+                    key_event = game_window.getch()
+
+                    if key_event == curses.ERR:
+                        await asyncio.sleep(0.1)
+                    elif key_event == curses.KEY_UP:
+                        act = 'rotate'
+                    elif key_event == curses.KEY_DOWN:
+                        act = 'down'
+                    elif key_event == curses.KEY_LEFT:
+                        act = 'left'
+                    elif key_event == curses.KEY_RIGHT:
+                        act = 'right'
+                    elif key_event == ord(" "):
+                        act = 'drop'
+                    
+                await ws.send(json.dumps({'event': 'act', 'act': act}))
+            elif e == 'gameover':
+                curses.endwin()
+                break
+
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    curses.endwin()
