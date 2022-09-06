1ea9c863cf3009e3e9c9bf0414c8436b63525a4e
matt <matt76k@gmail.com>
2022-09-07 08:50:22 +0900

    Add description

--- Bot/Player.py
+++ Bot/Player.py
@@ -6,5 +6,16 @@ class Player:
         self.name = name
     
     def act(self, info):
+        """
+        infoはlistになっていて、最初が自分の情報、それ以外は相手の情報があります。
+        情報は、5つあり
+            board: 確定しているボード(2次元リスト)
+            next: 次のブロック3つ（IやOといった形）
+            score:現時点でのスコア
+            block: 現在操作しているブロックの形
+            shape: 現在操作しているブロックの形状
+            pos: 現在操作しているブロックの位置
+        """
+
         return random.choice(ACTIONS)
 
