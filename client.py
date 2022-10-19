9c00793d5b2660286ff8a4e060ebd0b522b5792c
matt <matt76k@gmail.com>
2022-10-19 12:16:46 +0900

    ADD devnull option

--- client.py
+++ client.py
@@ -4,12 +4,15 @@ import json
 import argparse
 import socket
 import importlib
+import sys
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--host', type=str, default=socket.gethostname())
 parser.add_argument('--port', type=int, default=8765)
-parser.add_argument('--timeout', type=int, default=60*10)
+parser.add_argument('--timeout', type=int, default=60*2)
 parser.add_argument('--player', type=str, default="Bot")
+parser.add_argument('--devnull', action='store_true')
 
 args = parser.parse_args()
 player = getattr(importlib.import_module(f"{args.player}.Player"), "Player")(args.player)
@@ -18,8 +21,6 @@ score = 0
 
 async def play():
     global score, executor
-
-    loop = asyncio.get_event_loop()
 
     async with websockets.connect(f"ws://{args.host}:{args.port}") as ws:
         await ws.send(json.dumps({'event': 'join', 'name': player.name}))
@@ -40,9 +41,13 @@ if __name__ == "__main__":
     asyncio.set_event_loop(loop)
 
     try:
-        loop.run_until_complete(asyncio.wait_for(play(), args.timeout))
+        with open(os.devnull, 'w') as t:
+            if args.devnull:
+                sys.stdout = t
+            loop.run_until_complete(asyncio.wait_for(play(), args.timeout))
     except:
         pass
     finally:
+        sys.stdout = sys.__stdout__
         print(f"score = {score}", flush=True)
         loop.close()
