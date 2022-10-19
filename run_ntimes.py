85cbf930fde38e9c6e364c39ddfd8a1710167b96
matt <matt76k@gmail.com>
2022-10-19 12:17:48 +0900

    ADD ntimes option

--- run_ntimes.py
+++ run_ntimes.py
@@ -1,21 +1,27 @@
 import subprocess
 import time
-import os
-import signal
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--player', type=str, default="Bot")
+parser.add_argument('--ntimes', type=int, default=3)
+parser.add_argument('--timeout', type=int, default=10)
+
+args = parser.parse_args()
 
 scmd = "python3 ai_mode.py --nodisplay"
-ccmd = "python3 client.py --timeout 120"
+ccmd = f"python3 client.py --timeout {args.timeout} --player {args.player} --devnull"
 
 server = subprocess.Popen(scmd.split())
 
 if __name__ == "__main__":
     try:
         score = []
-        for i in range(10):
-            time.sleep(0.1)
+        for i in range(args.ntimes):
+            time.sleep(0.2)
             res = subprocess.run(ccmd.split(), stdout=subprocess.PIPE, check=True).stdout.decode('utf-8').split()[2]
             score.append(int(res))
 
-        print(sum(score) / 10.0)
+        print(sum(score) / float(args.ntimes))
     finally:
         server.terminate()
