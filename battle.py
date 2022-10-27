260691ed2f0c755879e1c17653215dd5eeca5d89
matt <matt76k@gmail.com>
2022-10-27 16:25:34 +0900

    FIXED for old python version

--- battle.py
+++ battle.py
@@ -118,13 +118,14 @@ async def main():
         await asyncio.Future()
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    import warnings
+    warnings.filterwarnings('ignore')
 
     if args.nodisplay:
         init_render()
 
     try:
+        loop = asyncio.get_event_loop()
         loop.run_until_complete(asyncio.wait_for(main(), args.timeout))
     except:
         if args.nodisplay:
